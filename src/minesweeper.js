fda05995edd0498725b6eeb6e4ec13d0a75f8fce
Ryoji Matsuzaki <ryoji6464@gmail.com>
2018-08-21 13:47:38 +0300

    added following features: 1. display the number of bombs adjacent to the flipped tile 2. allow the user to flip the tile

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -55,7 +55,7 @@ printBoard(bombBoard);
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   const board = [];
   for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
-    const row =[];
+    const row = [];
     for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
       row.push(' ');
     };
@@ -82,39 +82,66 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
 
     if (board[randomRowIndex][randomColumnIndex] !== 'B') {
-    //now that we have a random row and a column, we want to put a bomb there
-    board[randomRowIndex][randomColumnIndex] = 'B';
-    //this line below makes sure that the while loop will eventually end
-    numberOfBombsPlaced++;
+      //now that we have a random row and a column, we want to put a bomb there
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      //this line below makes sure that the while loop will eventually end
+      numberOfBombsPlaced++;
 
     };
   };
   return board;
 };
 
+//this portion will display the number of bombs adjacent to the flipped tile
 let getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   let neighborOffsets = [
-    [rowOffset, columnOffset],
-    [rowOffset, columnOffset],
-    [rowOffset, columnOffset],
-    [rowOffset, columnOffset],
-    [rowOffset, columnOffset],
-    [rowOffset, columnOffset],
-    [rowOffset, columnOffset],
-    [rowOffset, columnOffset],
-    ];
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+  ];
   const numberOfRows = bombBoard.length;
-  const numberOfColumns = bombBoard.length.
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+    if (neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+      if (bombBoard[neighborRowIndex][neighborColumnIndex] == 'B') {
+        numberOfBombs++;
+      }
+    };
+  });
+  return numberOfBombs;
 };
 
+//this portion will allow the user to flip the tile
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  if (playerBoard[rowIndex][columnIndex] !== ' ') {
+    console.log('This tile has already been flipped!');
+    return;
+  } else if (bombBoard[rowIndex][columnIndex] == 'B') {
+      playerBoard[rowIndex][columnIndex] == 'B';
+  } else {
+      playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+  }
+}; 
 
 
 const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
 }
 
-const playerBoard = generatePlayerBoard(3,3);
-const bombBoard = generateBombBoard(3,3,2);
+const playerBoard = generatePlayerBoard(3, 3);
+const bombBoard = generateBombBoard(3, 3, 2);
 
+flipTile(playerBoard, bombBoard, 0, 0);
+console.log('Updated Player Board: ')
 printBoard(playerBoard);
+console.log('Bomb Board: ')
 printBoard(bombBoard);
