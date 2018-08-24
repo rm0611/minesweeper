f6c749e7893ed7e3282b6dd916d76a2d93a70d35
Ryoji Matsuzaki <ryoji6464@gmail.com>
2018-08-24 16:25:38 +0300

    added Board Class, moved fliptile function into Board Class

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,56 +1,28 @@
-/*
-//make the board of the player
-const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
-  let board = [];
-    for (let i = 0; i < numberOfRows; i++) {
-      let row = [];
-        for (let j = 0; j < numberOfColumns; j++) {
-          row.push(' ');
-        }
-        board.push(row);
+class Board {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+    this._numberOfBombs = numberOfBombs;
+    this._numberOfTiles = numberOfRows * numberOfColumns;
+    this._playerBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
+    this._bombBoard = Board.generatePlayerBoard(numberOfRows, numberOfColumns);
+  }
+  get playerBoard() {
+    return _playerBoard;
+  }
+
+  //this portion will allow the user to flip the tile
+  flipTile = (rowIndex, columnIndex) => {
+    if (this._playerBoard[rowIndex][columnIndex] !== ' ') {
+      console.log('This tile has already been flipped!');
+      return;
+    } else if (this._bombBoard[rowIndex][columnIndex] == 'B') {
+      this._playerBoard[rowIndex][columnIndex] == 'B';
+    } else {
+      this._playerBoard[rowIndex][columnIndex] = this.getNumberOfNeighborBombs(rowIndex, columnIndex);
     }
-    return board;
-};
-
-//make the board of the bomb that will be placed
-const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
-  let board = [];
-    for (let i = 0; i < numberOfRows; i++) {
-      let row = [];
-        for (let j = 0; j < numberOfColumns; j++) {
-          row.push(null);
-        }
-        board.push(row);
-    }
-    
-    //Represent the bomb counter
-    let numberOfBombsPlaced = 0;
-    while (numberOfBombsPlaced < numberOfBombs) {
-      //Generate random location on board
-      let randomRowIndex = Math.floor(Math.random() * numberOfRows);
-      let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-      board[randomRowIndex][randomColumnIndex] = 'B';
-      //note: this has the possibility to place bombs on top of existing bombs
-      numberOfBombsPlaced++;
-    }
-    return board;
-};
+  this._numberOfTiles--;
+}
 
 
-const printBoard = (board) => {
-  console.log(board.map(row => row.join(' | ')).join('\n'));
-};
-
-//test print the game board
-let playerBoard = generatePlayerBoard(5,5);
-let bombBoard = generateBombBoard(5,5,3);
-
-console.log('Player Board: ');
-printBoard(playerBoard);
-console.log('Bomb Board: ');
-printBoard(bombBoard);
-
-*/
 
 
 //This portion is to generate the board which the players will interact
@@ -122,18 +94,6 @@ let getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   });
   return numberOfBombs;
 };
-
-//this portion will allow the user to flip the tile
-const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
-  if (playerBoard[rowIndex][columnIndex] !== ' ') {
-    console.log('This tile has already been flipped!');
-    return;
-  } else if (bombBoard[rowIndex][columnIndex] == 'B') {
-      playerBoard[rowIndex][columnIndex] == 'B';
-  } else {
-      playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
-  }
-}; 
 
 //this portion is to print the actual board
 const printBoard = board => {
